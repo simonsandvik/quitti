cf9c81bb5798aaca6bf3856f860b05ea2d616fb2
Simon Sandvik <simonsandvik@MacBook-Pro-som-tillhor-Simon-2.local>
2026-01-29 23:45:23 +0200

    Fix: Relax cookie SameSite policy to fix Azure AD state mismatch

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -81,4 +81,31 @@ export const authOptions: NextAuthOptions = {
     },
     */
     debug: false,
+    cookies: {
+        sessionToken: {
+            name: `next-auth.session-token`,
+            options: {
+                httpOnly: true,
+                sameSite: 'lax',
+                path: '/',
+                secure: process.env.NODE_ENV === 'production',
+            },
+        },
+        callbackUrl: {
+            name: `next-auth.callback-url`,
+            options: {
+                sameSite: 'lax',
+                path: '/',
+                secure: process.env.NODE_ENV === 'production',
+            },
+        },
+        csrfToken: {
+            name: `next-auth.csrf-token`,
+            options: {
+                sameSite: 'lax',
+                path: '/',
+                secure: process.env.NODE_ENV === 'production',
+            },
+        },
+    },
 };
